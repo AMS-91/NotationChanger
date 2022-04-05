1ad6c6c2bbb87771b1610ef1ef177b17a55f2c12
DEVready <amsmutu@gmail.com>
2022-04-05 14:40:17 +0900

    Initial Commit

---
+++ NotationChanger/NotationChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotationChangerApp.swift
+//  NotationChanger
+//
+//  Created by 안민수 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct NotationChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
